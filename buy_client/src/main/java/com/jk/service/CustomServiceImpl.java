6a53ded73942bb93dae1065b865bf159d3d1c619
lzl <luran927@126.com>
2018-12-20 19:28:09 +0800

    买家 补充@Service

--- buy_client/src/main/java/com/jk/service/CustomServiceImpl.java
+++ buy_client/src/main/java/com/jk/service/CustomServiceImpl.java
@@ -2,7 +2,9 @@ package com.jk.service;
 
 import com.jk.mapper.CustomMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomServiceImpl implements CustomService{
 
     @Autowired

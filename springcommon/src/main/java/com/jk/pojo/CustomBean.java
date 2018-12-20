6668ce012d0f7f5121a1f545d892028a16c7bbd0
kqcer <1723052454@qq.con>
2018-12-20 19:36:04 +0800

    B-1-0定制产品

--- springcommon/src/main/java/com/jk/pojo/CustomBean.java
+++ springcommon/src/main/java/com/jk/pojo/CustomBean.java
@@ -2,22 +2,203 @@ package com.jk.pojo;
 
 public class CustomBean {
 
-    private String customId;
-    private String customName;
+    private String id;          //产品Id
 
-    public String getCustomId() {
-        return customId;
+    private String name;        //产品名称
+
+    private String count;       //产品数量
+
+    private String type;        //产品类型
+
+    private String minentrance; //最小进口压力
+
+    private String minenunit;    //最小进口压力单位
+
+    private String maxentrance; //最大进口压力
+
+    private String maxenunit;    //最大进口压力单位
+
+    private String minexport;   //最小出口压力
+
+    private String minexunit;   //最小出口压力单位
+
+    private String maxexport;   //最大出口压力
+
+    private String maxexunit;   //最大出口压力单位
+
+    private String flow;        //流量计长度
+
+    private String booster;     //调压器
+
+    private String brand;       //调压器品牌
+
+    private String carton;      //外箱
+
+    private String material;    //外箱材质
+
+    private String remarks;     //备注
+
+    private String accessory;   //附件
+
+    private String demand;      //需求
+
+    public String getId() {
+        return id;
     }
 
-    public void setCustomId(String customId) {
-        this.customId = customId;
+    public void setId(String id) {
+        this.id = id;
     }
 
-    public String getCustomName() {
-        return customName;
+    public String getName() {
+        return name;
     }
 
-    public void setCustomName(String customName) {
-        this.customName = customName;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMinentrance() {
+        return minentrance;
+    }
+
+    public void setMinentrance(String minentrance) {
+        this.minentrance = minentrance;
+    }
+
+    public String getMinenunit() {
+        return minenunit;
+    }
+
+    public void setMinenunit(String minenunit) {
+        this.minenunit = minenunit;
+    }
+
+    public String getMaxentrance() {
+        return maxentrance;
+    }
+
+    public void setMaxentrance(String maxentrance) {
+        this.maxentrance = maxentrance;
+    }
+
+    public String getMaxenunit() {
+        return maxenunit;
+    }
+
+    public void setMaxenunit(String maxenunit) {
+        this.maxenunit = maxenunit;
+    }
+
+    public String getMinexport() {
+        return minexport;
+    }
+
+    public void setMinexport(String minexport) {
+        this.minexport = minexport;
+    }
+
+    public String getMinexunit() {
+        return minexunit;
+    }
+
+    public void setMinexunit(String minexunit) {
+        this.minexunit = minexunit;
+    }
+
+    public String getMaxexport() {
+        return maxexport;
+    }
+
+    public void setMaxexport(String maxexport) {
+        this.maxexport = maxexport;
+    }
+
+    public String getMaxexunit() {
+        return maxexunit;
+    }
+
+    public void setMaxexunit(String maxexunit) {
+        this.maxexunit = maxexunit;
+    }
+
+    public String getFlow() {
+        return flow;
+    }
+
+    public void setFlow(String flow) {
+        this.flow = flow;
+    }
+
+    public String getBooster() {
+        return booster;
+    }
+
+    public void setBooster(String booster) {
+        this.booster = booster;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getCarton() {
+        return carton;
+    }
+
+    public void setCarton(String carton) {
+        this.carton = carton;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getAccessory() {
+        return accessory;
+    }
+
+    public void setAccessory(String accessory) {
+        this.accessory = accessory;
+    }
+
+    public String getDemand() {
+        return demand;
+    }
+
+    public void setDemand(String demand) {
+        this.demand = demand;
     }
 }

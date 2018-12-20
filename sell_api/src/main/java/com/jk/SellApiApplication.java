d45ee6177b1d22ed90375226f53e89749e5fc493
lzl <luran927@126.com>
2018-12-20 15:20:05 +0800

    搭建项目, 配置了redis和 rabbitmq,目前是空项目,并且可以运行。

---
+++ sell_api/src/main/java/com/jk/SellApiApplication.java
@@ -0,0 +1,14 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellApiApplication.class, args);
+    }
+
+}
+

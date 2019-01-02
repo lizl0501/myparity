bbb0a4d901be7742fa0ea711a123932eb5fc8ea0
shenleiGOOD <3118281629@qq.com>
2019-01-02 14:30:48 +0800

    B14新增表

---
+++ springcommon/src/main/java/com/jk/pojo/PeiZhi.java
@@ -0,0 +1,31 @@
+package com.jk.pojo;
+
+public class PeiZhi {
+    private String bid;
+    private String leimuname;//类目
+    private String bname;//物品名称
+
+    public String getBid() {
+        return bid;
+    }
+
+    public void setBid(String bid) {
+        this.bid = bid;
+    }
+
+    public String getLeimuname() {
+        return leimuname;
+    }
+
+    public void setLeimuname(String leimuname) {
+        this.leimuname = leimuname;
+    }
+
+    public String getBname() {
+        return bname;
+    }
+
+    public void setBname(String bname) {
+        this.bname = bname;
+    }
+}
